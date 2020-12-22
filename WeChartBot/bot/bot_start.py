40ef5b61a79dcd2161ba3392749fb5c40aaeb5a2
wangt <wangt@qq.com>
2020-12-22 16:35:55 +0800

    update

--- WeChartBot/bot/bot_start.py
+++ WeChartBot/bot/bot_start.py
@@ -21,14 +21,15 @@ admin_wx = 'wxid_dg5xnz4s39ea21'
 # 单人黑名单列表
 single_block_list = ['wxid_xxxx']  # 最好把控制台微信加进去
 # 群组接受名单
-group_receive_list = ["19162403962@chatroom",#测试1群
-                      "17648533871@chatroom", #测试2群
-                      "18162133106@chatroom",#大娃去哪了
-                      "5581512142@chatroom",#顺其自然
-                      "7165888394@chatroom",#家人2（年轻人）
-                      "7029811144@chatroom", #人类精英交流群
-                      "20209106528@chatroom",# SC中台组
-                      '20086915165@chatroom', #鲧禹中台大群
+group_receive_list = [
+                    #   "19162403962@chatroom",  # 测试1群
+                    #   "17648533871@chatroom",  # 测试2群
+                    #   "18162133106@chatroom",  # 大娃去哪了
+                    #   "5581512142@chatroom",  # 顺其自然
+                    #   "7165888394@chatroom",  # 家人2（年轻人）
+                    #   "7029811144@chatroom",  # 人类精英交流群
+                    #   "20209106528@chatroom",  # SC中台组
+                    #   '20086915165@chatroom',  # 鲧禹中台大群
                       ]
 # 创建remark_name字典
 dict_remark_name = {}
@@ -141,12 +142,12 @@ def thread_handle_message(wx_inst):
                     'data', {}).get('from_chatroom_wxid', '')
                 print('接受群组信息1:', group_receive_list,
                       (from_chatroom_wxid in group_receive_list), send_or_recv, send_or_recv[0])
+                
                 if send_or_recv[0] == '0':
                     if from_chatroom_wxid in group_receive_list:
                         print('接受群组信息2:', send_or_recv,
                               send_or_recv[0], 'from_chatroom_wxid', from_chatroom_wxid, 'data_type[0] :', data_type[0])
                         if data_type[0] == '1':
-                            print('发送消息--->>>')
                             msg_content = message.get(
                                 'data', {}).get('msg', '')
                             from_wxid = message.get('data', {}).get(
@@ -155,8 +156,6 @@ def thread_handle_message(wx_inst):
                                 'data', {}).get('from_chatroom_wxid', '')
                             from_chatroom_nickname = message.get(
                                 'data', {}).get('from_chatroom_nickname', '')
-                            print('发送消息:', '微信收到群 {} 消息{} : {}  信息ID {}'.format(from_chatroom_nickname,
-                                                                                dict_remark_name.get(from_wxid, from_wxid), msg_content, ID_num))
                             #wx_inst.send_text(admin_wx, '微信收到群 {} 消息\n {} : {}  \n信息ID {}'.format(from_chatroom_nickname,dict_remark_name.get(from_wxid, from_wxid), msg_content, ID_num))
                             if re.match(r'^[舔,赞,顶,捧,夸]+.+', msg_content):
                                 content_name = re.match(
@@ -172,6 +171,27 @@ def thread_handle_message(wx_inst):
                                     "https://chp.shadiao.app/api.php")
                                 wx_inst.send_text(
                                     from_chatroom_wxid, "@{}  {}".format(content_name, response.text))
+                            
+                            elif re.match(r'.*[点歌]+.+', msg_content):
+                                content_text = re.match(
+                                    r'.*[点歌]+(.+)', msg_content).group(1).replace("@", "").replace("?", "")
+                                print("点歌：", content_text)
+                                response = requests.get(
+                                    "https://c.y.qq.com/soso/fcgi-bin/client_search_cp?p=1&n=1&w="+content_text+"&format=json")
+                                text_json = json.loads(response.text)
+                                print("text_json：", text_json)
+                                if text_json.get("data", {}).get("song").get("totalnum") <= 0:
+                                    wx_inst.send_text(
+                                        from_chatroom_wxid, "未找到歌曲【"+content_text+"】")
+                                obj = text_json.get("data", {}).get(
+                                    "song", {}).get("list", {})[0]
+                                print("obj:", obj)
+                                albummid = obj.get(
+                                    "albummid", "002vVK9r4dRzPY")
+                                if albummid == "":
+                                    albummid = "002vVK9r4dRzPY"
+                                wx_inst.send_link_card(from_chatroom_wxid, obj.get("songname"), obj.get("singer")[0].get("name"), "https://y.qq.com/n/yqq/song/"+obj.get(
+                                    "songmid", "")+".html", img_url='https://y.gtimg.cn/music/photo_new/T002R300x300M000'+albummid+'_1.jpg?max_age=2592000')
                             elif re.match(r'^[喷,骂]+.+', msg_content) and from_wxid == admin_wx:
                                 # https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn
                                 content_name = re.match(
@@ -180,8 +200,10 @@ def thread_handle_message(wx_inst):
                                     "https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn")
                                 wx_inst.send_text(
                                     from_chatroom_wxid, "@{}  {}".format(content_name, response.text))
+                                                             
                             elif re.match(r'[help,帮助]+.*@天天bot[\?]*[help,帮助]+', msg_content):
-                                help_msg='''天气：@bot天气深圳
+                                help_msg = '''赞某人：舔xx，赞xx，顶xx，捧xx，夸xx,xx真棒，xx真强，xx真厉害，xx真牛逼
+天气：@bot天气深圳
 中英翻译：@bot翻译i love you
 智能聊天：@bot你好
 笑话：@bot笑话
@@ -206,7 +228,7 @@ def thread_handle_message(wx_inst):
                                     "http://api.qingyunke.com/api.php?key=free&appid=0&msg="+to_my_msg)
                                 print(response.text)
                                 wx_inst.send_text(
-                                    from_chatroom_wxid, "{}".format(json.loads(response.text).get('content','').replace('{br}','\n')))
+                                    from_chatroom_wxid, "{}".format(json.loads(response.text).get('content', '').replace('{br}', '\n')))
                         else:
                             from_wxid = message.get('data', {}).get(
                                 'from_member_wxid', '')
@@ -220,6 +242,32 @@ def thread_handle_message(wx_inst):
                         # 弄一个字典, 保存信息ID, 通过回复ID+信息进行回复给好友
                         dict_msg_ID[ID_num] = from_chatroom_wxid
                         ID_num = ID_num + 1
+                    ## 不在权限群组的消息
+                    else:
+                        if data_type[0] == '1':
+                            msg_content = message.get(
+                            'data', {}).get('msg', '')
+                            from_wxid = message.get('data', {}).get(
+                                'from_member_wxid', '')
+                            from_chatroom_wxid = message.get(
+                                'data', {}).get('from_chatroom_wxid', '')
+                            from_chatroom_nickname = message.get(
+                            'data', {}).get('from_chatroom_nickname', '')
+                            ## 所有群组权限
+                            if re.match(r'^[添加群组]+', msg_content) and from_wxid == admin_wx :
+                                msg_content = message.get('data', {}).get('msg', '')
+                                from_wxid = message.get('data', {}).get('from_member_wxid', '')
+                                from_chatroom_wxid = message.get('data', {}).get('from_chatroom_wxid', '')
+                                from_chatroom_nickname = message.get('data', {}).get('from_chatroom_nickname', '')
+                                from_chatroom_nickname = message.get('data', {}).get('from_chatroom_nickname', '')
+                                #global group_receive_list
+                                print('添加群组0',from_chatroom_wxid)
+                                group_receive_list.append(from_chatroom_wxid)
+                                print('添加群组1',group_receive_list,{from_chatroom_wxid:from_chatroom_nickname})
+                                add_group_receive_config({from_chatroom_wxid:from_chatroom_nickname})
+                                print('添加群组2',{from_chatroom_wxid:from_chatroom_nickname})
+                                wx_inst.send_text(from_chatroom_wxid, "群组【"+from_chatroom_nickname+"】添加成功！") 
+
         except Exception as e:
             print(e, '消息处理失败!', message)
             pass
@@ -282,8 +330,30 @@ def thread_handle_member_welcome(wx_inst, member_groups):
             pass
 
 
+def get_config_path():
+    return 'D:\Project\Bot\WechatPCAPI\WeChartBot\\bot\config.json'
+
+
+def add_group_receive_config(dic):
+    path = get_config_path()
+    try:
+        f = open(get_config_path(), "rb")
+        j = json.loads(f.read())
+        j.get('group_receive_list_dic', [{}]).append(dic)
+        f.close()
+        config_writer(json.dumps(j,ensure_ascii=False))
+    except Exception as e:
+        print(e)
+
+
+def config_writer(txt):
+    f = open(get_config_path(), "w" ,encoding='utf-8')
+    f.write(txt)
+    f.close()
+
+
 def load_config():
-    path = 'D:\Project\Bot\WechatPCAPI\src\config.json'
+    path = get_config_path()
     print(path)
     # while True:
     try:
@@ -299,16 +369,23 @@ def load_config():
             # # 定义信息ID字典
             # dict_msg_ID = {}
             j = json.loads(f.read())
+            global admin_wx
             admin_wx = j.get('admin_wx', 'wxid_dg5xnz4s39ea21')
+            global single_block_list
             single_block_list = j.get('single_block_list', ['wxid_xxxx'])
-            group_receive_list = j.get('group_receive_list', [
-                '19162403962@chatroom'])
+           # global group_receive_list
+            for item in  j.get('group_receive_list_dic', [{}]):
+                group_receive_list.append(list(item.keys())[0])
+            #group_receive_list = j.get('group_receive_list_dic', [{}]).keys()
+            print(group_receive_list)
+            global dict_remark_name
             dict_remark_name = j.get('dict_remark_name', {})
+            global dict_msg_ID
             dict_msg_ID = j.get('dict_msg_ID', {})
             print('重新加载配置：', j)
-            print(admin_wx, single_block_list, group_receive_list,
+            print("配置打印",admin_wx, single_block_list, group_receive_list,
                   dict_remark_name, dict_msg_ID)
-            time.sleep(10)
+            #time.sleep(10)
     except Exception as e:
         print(e)
 
@@ -318,7 +395,7 @@ def main():
     # threading.Thread(target=load_config,
     #                  args=()).start()
     # print("配置加载完成！")
-   # load_config()
+    load_config()
     wx_inst = WechatPCAPI(on_message=onmessage)
     wx_inst.start_wechat(block=True)
     time.sleep(3)
